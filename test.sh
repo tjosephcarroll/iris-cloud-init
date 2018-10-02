74750542f127865b22098ad8dff4e86b5ae4196b
tjosephcarroll <tjosephcarroll@gmail.com>
2018-10-02 16:53:13 -0500

    retry

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo rm -r /var/lib/cloud/scripts/per-boot/
-sudo cp ../init.sh /var/lib/cloud/scripts/per-boot/init.sh
+sudo rm ISC.sh;sudo rm README.md; sudo rm test.sh
+#sudo cp ../init.sh /var/lib/cloud/scripts/per-boot/init.sh
 sudo rm -rf /var/lib/cloud/instance /var/lib/cloud/instances/*; sudo cloud-init modules -m final
