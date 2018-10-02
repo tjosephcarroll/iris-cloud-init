dcfdefca6df069e442a584dae085b28c5d2a6d0d
tjosephcarroll <tjosephcarroll@gmail.com>
2018-10-02 16:25:33 -0500

    test script

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo rm -r /var/lib/cloud/scripts/per-boot/
+sudo cp init.sh /var/lib/cloud/scripts/per-boot/init.sh
+sudo rm -rf /var/lib/cloud/instance /var/lib/cloud/instances/*; sudo cloud-init modules -m final
