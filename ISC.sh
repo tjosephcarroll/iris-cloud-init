b5113fba1b86a2108bb5122390184d271c6e788d
tjosephcarroll <tjosephcarroll@gmail.com>
2018-10-02 16:18:00 -0500

    check for docker

--- ISC.sh
+++ ISC.sh
@@ -1,15 +1,44 @@
 #!/bin/sh
+#
+#MAIN
+#
+
+#Docker?
+which docker
+if [ $? -eq 0 ]
+then
+    docker --version | grep "Docker version"
+    if [ $? -eq 0 ]
+    then
+        echo "docker exists"
+    else
+        echo "install docker"
+        install_docker
+    fi
+else
+    echo "install docker"
+    install_docker
+fi
+
+
+
+
+
+
 #Install docker
-sudo apt update -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update -y
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-sudo service docker start
-sudo usermod -aG docker ${USER}
+function install_docker() {
 
-#configure docker
+    sudo apt update -y
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    sudo apt update -y
+    apt-cache policy docker-ce
+    sudo apt install docker-ce -y
+    sudo service docker start
+    sudo usermod -aG docker ${USER}
 
+    #configure docker
+
+}
 echo "hello world" >/tmp/test1
