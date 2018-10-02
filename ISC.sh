47d4d379d11482fbfc465b66a308ca36b6db39d8
tjosephcarroll <tjosephcarroll@gmail.com>
2018-10-02 16:02:23 -0500

    initial commit

---
+++ ISC.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#Install docker
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo service docker start
+sudo usermod -aG docker ${USER}
+
+#configure docker
+
+echo "hello world1" >/tmp/test
